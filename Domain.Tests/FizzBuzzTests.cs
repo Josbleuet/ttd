b32ae3394c8d898fefa14652a999b3f5f8a78d89
eric.fortin <eric.fortin@cauca.ca>
2020-07-02 16:05:49 -0400

    FizzBuzz original

---
+++ Domain.Tests/FizzBuzzTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace Domain.Tests
+{
+    public class FizzBuzzTests
+    {
+        private FizzBuzz fizzBuzz;
+
+        private static readonly int valueMultipleOf3 = 3;
+        private static readonly int valueMultipleOf5 = 5;
+        private static readonly int valueMultipleOf3And5 = 15;
+        private static readonly int valueNotMultipleOf3Or5 = 7;
+
+        [SetUp]
+        public void Setup()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        [Test]
+        public void Value_WhenIsMultipleOf3_ShouldReturnFizz()
+        {
+            int value = valueMultipleOf3;
+
+            var result = fizzBuzz.GetResult(value);
+
+            Assert.That(result, Is.EqualTo(FizzBuzz.FizzResult));
+        }
+
+        [Test]
+        public void Value_WhenIsMultipleOf5_ShouldReturnBuzz()
+        {
+            int value = valueMultipleOf5;
+
+            var result = fizzBuzz.GetResult(value);
+
+            Assert.That(result, Is.EqualTo(FizzBuzz.BuzzResult));
+        }
+
+        [Test]
+        public void Value_WhenIsMultipleOf3And5_ShouldReturnFizzBuzz()
+        {
+            int value = valueMultipleOf3And5;
+
+            var result = fizzBuzz.GetResult(value);
+
+            Assert.That(result, Is.EqualTo(FizzBuzz.FizzBuzzResult));
+        }
+
+        [Test]
+        public void Value_WhenIsNotMultipleOf3Or5_ShouldReturnValue()
+        {
+            int value = valueNotMultipleOf3Or5;
+
+            var result = fizzBuzz.GetResult(value);
+
+            Assert.That(result, Is.EqualTo(value.ToString()));
+        }
+
+    }
+}
---
+++ Domain/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Domain
+{
+    public class FizzBuzz
+    {
+        public static readonly string FizzResult = "Fizz";
+        public static readonly string BuzzResult = "Buzz";
+        public static readonly string FizzBuzzResult = "FizzBuzz";
+
+
+        public string GetResult(int value)
+        {
+            if (valueIsMultipleOf(value, 3) && valueIsMultipleOf(value, 5))
+            {
+                return FizzBuzzResult;
+            }
+            if (valueIsMultipleOf(value, 3))
+            {
+                return FizzResult;
+            }
+            if (valueIsMultipleOf(value, 5))
+            {
+                return BuzzResult;
+            }
+
+            return value.ToString();
+        }
+
+        private bool valueIsMultipleOf(int value, int multiple)
+        {
+            return value % multiple == 0;
+        }
+    }
+}
